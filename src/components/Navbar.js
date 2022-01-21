e11dbd120d7bbe4ff06394052e69d9d7e6381715
Jenna Qian <jiayin1996@gmail.com>
2022-01-21 09:00:07 -0500

    Added Navbar Component

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="Navbar">
+      <h1>
+        <Link to="/budget">Budget App</Link>
+      </h1>
+      <h1>
+      <Link to="/budget/new"><button>New Transaction</button></Link>
+      </h1>
+    </nav>
+  );
+}
