579595b7f6fd380d03e330bbfb0c31bfc883daba
Jenna Qian <jiayin1996@gmail.com>
2022-01-25 12:45:33 -0500

    minor changes

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,50 +1,65 @@
-import {useState, useEffect} from "react"
-import Transaction from "./Transaction.js"
-import axios from "axios"
+import { useState, useEffect } from "react";
+import Transaction from "./Transaction.js";
+import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL
+const API_URL = process.env.REACT_APP_API_URL;
 
-function Transactions () {
-    const [transactions, setTransactions] = useState([])
-    // console.log(API_URL)
-    useEffect(()=> {
-        axios.get(API_URL + "/transactions")
-        .then((res)=> {
-            // console.log(res.data)
-            setTransactions(res.data)
-        }).catch((err) =>{
-            throw err;
-        })
-    }, [])
+function Transactions() {
+  const [transactions, setTransactions] = useState([]);
+  // console.log(API_URL)
+  useEffect(() => {
+    axios
+      .get(API_URL + "/transactions")
+      .then((res) => {
+        // console.log(res.data)
+        setTransactions(res.data);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, []);
 
-    let accountTotal = transactions.map((elem)=> Number(elem.amount)).reduce((accu, currentVal) => {
-        return accu + currentVal}, 0)
-    return(
-        <div className="transactions-background">
-            {/* <div className="totalAmt-container"> */}
-                <h3 className="totalAmt-container">Total: ${accountTotal}</h3>
-            {/* </div> */}
-            <section className="transactionsTable">
-                <table className="table">
+  let accountTotal = transactions
+    .map((elem) => Number(elem.amount))
+    .reduce((accu, currentVal) => {
+      return accu + currentVal;
+    }, 0);
 
-                    <thead>
-                        <tr>
-                            <th>Date:</th>
-                            <th>From:</th>
-                            <th>Type:</th>
-                            <th>Amount:</th>
-                        </tr>
-                    </thead>
+//   let negOrPos = () => {
+//     if (accountTotal < 0) {
+//         return <h3 className="totalAmt-container">Total: ${accountTotal}</h3>
+//     }
+//   };
+  return (
+    <div className="transactions-background">
+      {/* <div className="totalAmt-container"> */}
+      <h3 className="totalAmt-container">Total: ${accountTotal}</h3>
+      {/* </div> */}
+      <section className="transactionsTable">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Date:</th>
+              <th>From:</th>
+              <th>Type:</th>
+              <th>Amount:</th>
+            </tr>
+          </thead>
 
-                    <tbody>
-                        {transactions.map((transaction, index) => {
-                            return <Transaction key={index} transaction={transaction} index={index}/>
-                        })}
-                    </tbody>
-
-                </table>
-            </section>
-        </div>
-    )
+          <tbody>
+            {transactions.map((transaction, index) => {
+              return (
+                <Transaction
+                  key={index}
+                  transaction={transaction}
+                  index={index}
+                />
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
 }
-export default Transactions
+export default Transactions;
